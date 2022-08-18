d2ecb50dcabed178bf5e6aa57bd6a1474960b862
mathiore <mraimundo@edu.univali.br>
2022-08-18 14:14:49 -0300

    styling experience done

--- src/components/experiencia/Experiencia.jsx
+++ src/components/experiencia/Experiencia.jsx
@@ -13,13 +13,88 @@ const Experiencia = () => {
           <h3>Desenvolvimento Front-End</h3>
           <div className="experience_content">
             <article className="experience_details">
-            <HiBadgeCheck/>
+            <HiBadgeCheck className='experience_details-icon'/>
+            <div>
+              <h4>HTML</h4>
+              <small>Experiente</small>
+            </div>
+            </article>
+            <article className="experience_details">
+            <HiBadgeCheck className='experience_details-icon'/>
+            <div>
+              <h4>CSS</h4>
+              <small>Experiente</small>
+            </div>
+            </article>
+            <article className="experience_details">
+            <HiBadgeCheck className='experience_details-icon'/>
+            <div>
+              <h4>JavaScript</h4>
+              <small>Intermediário</small>
+            </div>
+            </article>
+            <article className="experience_details">
+            <HiBadgeCheck className='experience_details-icon'/>
+            <div>
+              <h4>Bootsrap</h4>
+              <small>Experiente</small>
+            </div>
+            </article>
+            <article className="experience_details">
+            <HiBadgeCheck className='experience_details-icon'/>
+            <div>
+              <h4>React</h4>
+              <small>Intermediário</small>
+            </div>
+            </article>
+            <article className="experience_details">
+            <HiBadgeCheck className='experience_details-icon'/>
+            <div>
+              <h4>VueJS</h4>
+              <small>Básico</small>
+            </div>
             </article>
           </div>
         </div>
         <div className="experience_backend">
           <h3>Desenvolvimento Back-End</h3>
-
+          <div className="experience_content">
+          <article className="experience_details">
+            <HiBadgeCheck className='experience_details-icon'/>
+            <div>
+              <h4>NodeJs</h4>
+              <small>Intermediário</small>
+            </div>
+            </article>
+            <article className="experience_details">
+            <HiBadgeCheck className='experience_details-icon'/>
+            <div>
+              <h4>MongoDB</h4>
+              <small>Básico</small>
+            </div>
+            </article>
+            <article className="experience_details">
+            <HiBadgeCheck className='experience_details-icon'/>
+            <div>
+              <h4>PHP</h4>
+              <small>Básico</small>
+            </div>
+            </article>
+            <article className="experience_details">
+            <HiBadgeCheck className='experience_details-icon'/>
+            <div>
+              <h4>MySQL</h4>
+              <small>Básico</small>
+            </div>
+            </article>
+            <article className="experience_details">
+            <HiBadgeCheck className='experience_details-icon'/>
+            <div>
+              <h4>Python</h4>
+              <small>Intermediário</small>
+            </div>
+            </article>
+          </div>
         </div>
       </div>
     </section> 
